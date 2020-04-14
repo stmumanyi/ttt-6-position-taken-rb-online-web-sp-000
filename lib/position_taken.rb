3acb29649d1f3d23107f62328e5a88891367f0fb
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-14 16:08:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board[index] = " " || board[index] = "" || board
+  board[index] = " " || board[index] = "" || board[index] = 
 end
